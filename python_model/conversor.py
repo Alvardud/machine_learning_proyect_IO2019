33d900606ae83130f7a099a65c18ee7f4d99fd0e
Alvardud <alvaro_dudutex@outlook.es>
2019-05-27 17:50:16 -0400

    add model

---
+++ python_model/conversor.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+path="simpleKeras/"
+
+converter = tf.lite.TFLiteConverter.from_saved_model(path)
+tflite_model = converter.convert()
+open("tfLite/converted_model.tflite", "wb").write(tflite_model)
